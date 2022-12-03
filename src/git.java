e0b566065a0627e27ffa9229ec20347af8dc6490
Amin-tay <mohammadamintayebi1@gmail.com>
2022-12-03 23:11:00 +0330

    Change 2

--- src/git.java
+++ src/git.java
@@ -3,5 +3,6 @@ public class git {
         System.out.println("Primitive Version");
         System.out.println("Making a branch");
         System.out.println("Change 1");
+        System.out.println("Change 2");
     }
 }
